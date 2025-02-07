894d009c75b517e845a235604f98f2d02db19b19
gadewarvedant <vedantgadewar840@gmail.com>
2025-02-07 14:02:45 +0530

    Update LoginPage.jsx

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -16,7 +16,7 @@ const LoginPage = ({ setIsLoggedIn, setUserName, setEmail }) => {
     setErrors({}); // Clear any previous errors
 
     try {
-      const response = await fetch('http://localhost:5000/api/users/login', {
+      const response = await fetch('https://vehicleofvedant.onrender.com/api/users/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
